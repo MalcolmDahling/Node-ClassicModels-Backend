b045d10b5a44d1cc2578e53ca0e14a40b270b10b
MalcolmDahling <mac.dahling@gmail.com>
2022-05-31 19:44:50 +0200

    asdf

--- routes/cars.js
+++ routes/cars.js
@@ -8,24 +8,51 @@ router.use(cors());
 router.get('/categories', function(req, res, next) {
 
     req.app.locals.con.connect(function(err){
-        if(err) {
+        if(err){
             console.log(err);
         }
        
         let sql = `
-        SELECT productLine
-        FROM productLines
+            SELECT productLine
+            FROM productLines
         `;
 
         req.app.locals.con.query(sql, function (err, result){
-            if (err) {
-                console.log(err);
-            }
+            if (err){console.log(err);}
+            
             res.send(result);
         });
     });
 
 });
+
+
+
+router.post('/category', function(req, res){
+
+    req.app.locals.con.connect(function(err){
+        if(err){
+            console.log(err);
+        }
+
+        let sql = `
+            SELECT *
+            FROM products
+            WHERE productLine = '${req.body.category}'
+        `;
+
+        req.app.locals.con.query(sql, function(err, result){
+            if(err){console.log(err);}
+
+            res.send(result);
+        });
+    });
+
+});
+
+
+
+
 
 module.exports = router;
 
